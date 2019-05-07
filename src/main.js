c981002e43ed15710720556f116a106f8faeb270
yang <yang@yangs-iMac.local>
2019-05-07 15:40:55 +0800

    路由配置完成，开始制作布局页面

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from "vue"
 import App from "./App.vue"
-import router from "./router"
-import store from "./store"
+import router from "@/router/index.js"
+import store from "@/store/index.js"
 
 import "@/plugins/ant-design-vue.js"
 
@@ -10,5 +10,5 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
 }).$mount("#app")
